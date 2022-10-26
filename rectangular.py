f65a3e3e176623d379f05e463d477dd7d5a3ed23
boris387 <ivankovic.boris44@gmail.com>
2022-10-26 18:59:57 +0200

    Add files via upload
    Test repo

--- rectangular.py
+++ rectangular.py
@@ -1,18 +1,19 @@
-array = [3,4,5]
+array = [2,9,11,5,7,24,13,25,69]
 array_size = len(array)
 
+def multiplication(n):
+    return n * n
+
+array = sorted(list(map(multiplication, array)))
+
 def rectangular_triangle(array, x): #Takes two arguments, an array (list) and second argument
-	for i in range(x): # for every element in range
-		array[i] = array[i] * array[i] #  Square each element of the array
-	array.sort() # Sort the squared aray in ascending order
-
-
+	 #Map() iterates over the array and applies function to every element, then since Map() does not support sorted, it is converted to list and then sorted
 	for i in range(x-1, 1, -1):
 		j = 0
 		k = i - 1
 		while (j < k):
 			if (array[j] + array[k] == array[i]):
-				return True
+				return True, array[j], array[k], array[i]
 			else:
 				if (array[j] + array[k] < array[i]):
 					j = j + 1
@@ -21,4 +22,3 @@ def rectangular_triangle(array, x): #Takes two arguments, an array (list) and se
 	return False
 
 print(rectangular_triangle(array, array_size))
-
